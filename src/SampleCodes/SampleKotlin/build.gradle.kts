d3768aca3ef2943306a29ba0df2ba055cc2f038c
Oğuz Karan <oguzkaran@csystem.org>
2024-08-08 01:09:45 +0300

    Update

---
+++ src/SampleCodes/SampleKotlin/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    kotlin("jvm") version "2.0.0"
+}
+
+group = "org.csystem"
+version = "1.0.0"
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+}
+
+tasks.withType<Jar> {
+    manifest {
+        attributes["Main-Class"] = "org.csystem.app.AppKt"
+    }
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
---
+++ src/SampleCodes/SampleKotlin/src/main/kotlin/org/csystem/app/app.kt
@@ -0,0 +1,5 @@
+package org.csystem.app
+
+fun main() {
+    println("Hello, World")
+}
