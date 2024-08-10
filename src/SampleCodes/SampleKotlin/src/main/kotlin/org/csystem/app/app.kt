f80f8090a0811c1c1faf10f95ce5c77c4f34a120
Oğuz Karan <oguzkaran@csystem.org>
2024-08-10 13:08:44 +0300

    Update

--- src/SampleCodes/SampleKotlin/src/main/kotlin/org/csystem/app/app.kt
+++ src/SampleCodes/SampleKotlin/src/main/kotlin/org/csystem/app/app.kt
@@ -1,5 +1,12 @@
+/*----------------------------------------------------------------------------------------------------------------------
+    Kotlin 1.6 ile birlikte klavyeden (aslında stdin'dan) enter basılana kadar girilen yazıyı bir String olarak okuyan
+    readln fonkiyonu eklenmiştir
+----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app
 
 fun main() {
-    println("Hello, World")
+    print("Bir sayı giriniz:")
+    val a = readln().toInt()
+
+    println("${a * a}")
 }
