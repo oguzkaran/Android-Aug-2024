0f7d709f4e0e792e62cd34cbe5ad72577dff010c
Oğuz Karan <oguzkaran@csystem.org>
2024-08-18 11:13:36 +0300

    for study case

--- src/SampleCodes/SampleKotlin/src/main/kotlin/org/csystem/app/app.kt
+++ src/SampleCodes/SampleKotlin/src/main/kotlin/org/csystem/app/app.kt
@@ -1,15 +1,48 @@
 package org.csystem.app
 
-fun main() = runIsArmstrongTest()
+fun main() = runGetPrimeTest()
 
-fun runIsArmstrongTest() {
-    for (a in -10..99999)
-        if (isArmstrong(a))
-            println(a)
+fun runGetPrimeTest() {
+    while (true) {
+        print("Bir sayı giriniz:")
+        val n = readln().toInt()
+
+        if (n <= 0)
+            break
+
+        println("${n}. asal sayı: ${getPrime(n)}")
+    }
 
     println("Tekrar yapıyor musunuz?")
 }
 
-fun isArmstrong(a: Int): Boolean {
-    TODO("Test if a is an Armstrong number")
+fun getPrime(n: Int): Long {
+    TODO("Gets the nth prime number")
+}
+
+fun isPrime(a: Long): Boolean {
+    if (a <= 1)
+        return false
+
+    if (a % 2 == 0L)
+        return a == 2L
+
+    if (a % 3 == 0L)
+        return a == 3L
+
+    if (a % 5 == 0L)
+        return a == 5L
+
+    if (a % 7 == 0L)
+        return a == 7L
+
+    var i = 11L
+
+    while (i * i <= a) {
+        if (a % i == 0L)
+            return false
+        i += 2
+    }
+
+    return true
 }
