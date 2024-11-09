df1146056092f6a52d32f9d42a15f0e52a2aa00a
Oğuz Karan <oguzkaran@csystem.org>
2024-11-09 12:54:07 +0300

    update

--- src/SampleCodes/SampleKotlin/src/main/kotlin/org/csystem/app/app.kt
+++ src/SampleCodes/SampleKotlin/src/main/kotlin/org/csystem/app/app.kt
@@ -1,21 +1,5 @@
 package org.csystem.app
 
-import org.csystem.kotlin.util.console.readInt
-
 fun main() {
-    val a = readInt("Birinci sayıyı giriniz:")
-    val b = readInt("İkinci  sayıyı giriniz:")
 
-    println(sum(10, a, b))
-    println(sum(10))
-    println(sum(10, a))
-}
-
-fun sum(init: Int, vararg values: Int): Int {
-    var total = init
-
-    for (v in values)
-        total += v
-
-    return total
 }
